42b2b13d7ed4a72e51b39e6e0e126f478c5e4031
carl <thunman@gmail.com>
2014-04-26 17:51:24 -0400

    added all the different types of hands

--- Poker.py
+++ Poker.py
@@ -64,23 +64,113 @@ class Hand():
 		else:
 			return False
 
+	def has_four_of_a_kind(self):
+		if len(self.cards) == 5:
+			value_map = self.get_value_map()
+			if 4 in value_map.values():
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	def has_full_house(self):
+		if len(self.cards) == 5:
+			value_map = self.get_value_map()
+			if 3 in value_map.values() and 2 in value_map.values():
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	def has_set(self):
+		if len(self.cards) == 5:
+			value_map = self.get_value_map()
+			if 3 in value_map.values():
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	def has_two_pair(self):
+		if len(self.cards) == 5:
+			value_map = self.get_value_map()
+			count = 0
+			for value in value_map:
+				if value_map[value] == 2:
+					count += 1
+			print count
+			if count == 2:
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	def has_pair(self):
+		if len(self.cards) == 5:
+			value_map = self.get_value_map()
+			if 2 in value_map.values():
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	def get_value_map(self):
+		values = { 
+			2 : 0,
+			3 : 0,
+			4 : 0,
+			5 : 0,
+			6 : 0,
+			7 : 0,
+			8 : 0,
+			9 : 0,
+			10 : 0,
+			11 : 0,
+			12 : 0,
+			13 : 0,
+			14 : 0,
+		}
+		for card in self.cards:
+			if card['value'] == 'A':
+				values[14] += 1
+			else:
+				values[card['value']] += 1
+		return values
+
 def main():
 
-	card1 = { 'suit' : 'clubs', 'value' : 10 }
-	card2 = { 'suit' : 'clubs', 'value' : 11 }
-	card3 = { 'suit' : 'clubs', 'value' : 'A' }
-	card4 = { 'suit' : 'clubs', 'value' : 12 }
-	card5 = { 'suit' : 'clubs', 'value' : 13 }
+	card1 = { 'suit' : 'clubs', 'value' : 12 }
+	card2 = { 'suit' : 'hearts', 'value' : 11 }
+	card3 = { 'suit' : 'clubs', 'value' : 11 }
+	card4 = { 'suit' : 'clubs', 'value' : 10 }
+	card5 = { 'suit' : 'clubs', 'value' : 12 }
 
 	card_list = [ card1, card2, card3, card4, card5, ]
 	hand = Hand(card_list)
 
 	if hand.is_straight_flush():
 		print 'straight flush'
+	elif hand.has_four_of_a_kind():
+		print 'quads'
+	elif hand.has_full_house():
+		print 'full house'
 	elif hand.is_flush():
 		print 'flush'
 	elif hand.is_straight():
 		print 'straight'
+	elif hand.has_set():
+		print 'three of a kind'
+	elif hand.has_two_pair():
+		print 'two pair'
+	elif hand.has_pair():
+		print 'pair'
+
+	print hand.get_value_map()
 
 if __name__ == '__main__':
 	main()
