08c202ab9b55d3e4ab6d09e15fdf59f029ab72f6
carl <thunman@gmail.com>
2020-08-17 22:45:01 -0400

    fixing some bugs

--- KellyBet.py
+++ KellyBet.py
@@ -3,23 +3,17 @@ from decimal import Decimal
 
 # odds = b where bet pays out (b to 1)
 def kellyFraction(winProbability, odds):
-    # assert(type(winProbability) == type(Decimal))
-    # assert(type(odds) == type(Decimal))
 
     return max(((winProbability * (odds + 1)) - 1) / odds, 0)
 
 
 def kellyCall(betAmount, potSize, winProbability, bankroll):
-    # assert(type(betAmount) == type(Decimal))
-    # assert(type(potSize) == type(Decimal))
-    # assert(type(winProbability) == type(Decimal))
-    # assert(type(bankroll) == type(Decimal))
 
     f = Decimal(betAmount) / Decimal(bankroll)
-    odds = (potSize + betAmount) / betAmount
+    odds = Decimal(potSize + betAmount) / betAmount
     payout = ((winProbability * (odds + 1)) - 1) / odds
 
-    return payout / f > 1
+    return payout / f >= 1
 
 
 def kellyRaisePot(betAmount, potSize, winProbability, bankroll):
@@ -28,7 +22,7 @@ def kellyRaisePot(betAmount, potSize, winProbability, bankroll):
     f = Decimal(totalBet) / Decimal(bankroll)
     odds = 2
     payout = ((winProbability * (odds + 1)) - 1) / odds
-    return (payout / f) > 1
+    return (payout / f) >= 1
 
 
 def kellyAction(betAmount, potSize, winProbability, bankroll):
@@ -48,13 +42,13 @@ def main():
         'FOLD': '____'
     }
 
-    bankroll = Decimal(950)
+    bankroll = Decimal(10000)
     topRow = '\t'
     increment = Decimal('.05')
     for i in range(20):
         topRow += str(((i + 1) * increment) * 100) + '%\t'
     print(topRow)
-    for b in range(2, 60, 2):
+    for b in range(5, 95, 5):
         row = ''
         row += str((b)) + '\t'
         for i in range(1, 21):
@@ -67,11 +61,11 @@ def main():
                        bankroll=bankroll)]) + '\t'
         print(row)
 
-    for b in range(6, 21):
+    for b in range(10, 35):
         row = ''
-        row += str((b) * 10) + '\t'
+        row += str((b) * 20) + '\t'
         for i in range(1, 21):
-            bet = ((b) * 10)
+            bet = ((b) * 20)
             winProbability = ((i) * increment)
             row += str(actions[kellyAction(
                        betAmount=bet,
